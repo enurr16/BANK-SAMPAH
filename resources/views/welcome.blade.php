53b09b04c2bb0aea6304f2291614d588c9d380e6
ibnuu <ahmadibnu3471@gmail.com>
2026-08-02 11:16:20 +0700

    mengubah tampilan login dan logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,19 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-            @if (Route::has('login'))
-                <nav class="flex items-center justify-end gap-4">
+
+        </header>
+
+        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+            <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
+                <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
+                    <h1 class="mb-1 font-medium">Bank Sampah Digital</h1>
+                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Mari bersama menjaga lingkungan dengan. <br>mengelola sampah secara cerdas melalui sistem digital yang mudah, cepat, dan transparan</p>
+
+                    <br><br>
+
+                    @if (Route::has('login'))
+                <nav class="flex items-center justify-center gap-4">
                     @auth
                         <a
                             href="{{ url('/dashboard') }}"
@@ -44,13 +55,6 @@
                     @endauth
                 </nav>
             @endif
-        </header>
-        
-        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-            <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
-                <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
-                    <h1 class="mb-1 font-medium">Bank Sampah Digital</h1>
-                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Mari bersama menjaga lingkungan dengan. <br>mengelola sampah secara cerdas melalui sistem digital yang mudah, cepat, dan transparan</p>
                 </div>
             </main>
         </div>
